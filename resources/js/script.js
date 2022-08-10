72c2d8818570adb05288388a42f2ca61b23485f6
petersonprojetos <petersonrocha@gmail.com>
2022-08-10 15:40:21 -0300

    fase1
    primeiro teste

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.sub-btn').click(function(){
+        $(this).next('.sub-menu').slideToggle();
+    })
+})
+
+
+$(document).ready(function() {
+    $('.link-form').click(function(){
+        $(this).next('.form').slideToggle();
+    })
+})
